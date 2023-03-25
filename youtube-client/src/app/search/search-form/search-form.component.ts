f7a0de88f772c4c3cacd346f0fdc4533d6b7d13a
azozulya <bbbom@mail.ru>
2023-03-25 15:39:27 +0200

    feat: add header search form

--- youtube-client/src/app/search/search-form/search-form.component.ts
+++ youtube-client/src/app/search/search-form/search-form.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.scss']
+  styleUrls: ['./search-form.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SearchFormComponent {
 
+export class SearchFormComponent {
+  searchString: string = '';
+
+  onSearch(): void {
+    console.log(this.searchString);
+  }
 }

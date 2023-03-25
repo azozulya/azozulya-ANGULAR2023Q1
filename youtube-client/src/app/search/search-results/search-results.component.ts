e8cabbb8b50b56d9fb71d99174b7aeefe774a4a9
azozulya <bbbom@mail.ru>
2023-03-25 15:40:36 +0200

    feat: add movies list

--- youtube-client/src/app/search/search-results/search-results.component.ts
+++ youtube-client/src/app/search/search-results/search-results.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import mockDataList from "../../../data/data.json";
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss']
 })
-export class SearchResultsComponent {
 
+export class SearchResultsComponent {  
+  list = mockDataList.items;
 }
+

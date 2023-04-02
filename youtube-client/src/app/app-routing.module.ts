3d749b649ae4e918c4c68fa8e6af9a4c5b2450d8
azozulya <bbbom@mail.ru>
2023-04-02 16:51:27 +0300

    feat: add auth guard

--- youtube-client/src/app/app-routing.module.ts
+++ youtube-client/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { MainComponent } from './youtube/pages/main/main.component';
 import { DetailsComponent } from './youtube/pages/details/details.component';
 import { LoginComponent } from './auth/pages/login/login.component';
 import { NotFoundComponent } from './auth/pages/not-found/not-found.component';
+import { LoginGuard } from './auth/guards/login.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', component: MainComponent, pathMatch: 'full' },
-  { path: 'main/:search', component: MainComponent },
-  { path: '404', component: NotFoundComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'details', component: DetailsComponent },
+  { path: 'main', component: MainComponent, pathMatch: 'full', canActivate: [LoginGuard] },
+  { path: 'main/:search', component: MainComponent, canActivate: [LoginGuard] },
+  { path: 'details', component: DetailsComponent, canActivate: [LoginGuard] },
+  { path: '404', component: NotFoundComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 

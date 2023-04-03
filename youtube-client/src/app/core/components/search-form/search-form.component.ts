76c0367d233306c68a3d1971db02310ff72aece4
azozulya <bbbom@mail.ru>
2023-04-03 21:08:46 +0300

    fix: add redirect to main page after search

--- youtube-client/src/app/core/components/search-form/search-form.component.ts
+++ youtube-client/src/app/core/components/search-form/search-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
 
 @Component({
@@ -11,9 +12,12 @@ import { DataService } from '../../services/data.service';
 export class SearchFormComponent {
   searchString: string = '';
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private router: Router) { }
 
   onSearch(): void {
     this.dataService.searchMovies(this.searchString);
+
+    if (this.router.url !== '/main')
+      this.router.navigateByUrl('main');    
   }
 }

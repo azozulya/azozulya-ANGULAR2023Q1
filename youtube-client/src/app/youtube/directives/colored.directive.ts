8e51ed327d1e120c27c02b690b10e6cf38cd57dc
azozulya <bbbom@mail.ru>
2023-04-05 09:44:24 +0300

    refactor: the same colors logic in details page and movie list

--- youtube-client/src/app/youtube/directives/colored.directive.ts
+++ youtube-client/src/app/youtube/directives/colored.directive.ts
@@ -6,9 +6,15 @@ import { Directive, HostBinding, OnInit, Input } from '@angular/core';
 
 export class ColoredDirective implements OnInit {
   private colorClass!: string;
+  private colors = {
+    old: 'red',
+    older: 'yellow',
+    middle: 'green',
+    young: 'blue',
+  } 
+
   @Input() date!: string;  
 
-  @HostBinding('style.border-color') borderColor!:string;
   @HostBinding('class') get hostClasses() {
     return this.colorClass;
   }
@@ -19,22 +25,14 @@ export class ColoredDirective implements OnInit {
     return date > d;
   }
 
-  private colors = {
-    old: 'red',
-    older: 'yellow',
-    middle: 'green',
-    young: 'blue',
-  } 
-
   ngOnInit(): void {
     const currentDate = new Date(this.date);    
     
     if(this.checkDate(currentDate, 1)){
-      this.borderColor = this.checkDate(currentDate, 7, 'day') ? this.colors.young : this.colors.middle; 
-    } else {
-      this.borderColor = this.checkDate(currentDate, 6) ? this.colors.older : this.colors.old; 
-    }    
-    this.colorClass = this.borderColor;
-    console.log(this.colorClass);
+      this.colorClass = this.checkDate(currentDate, 7, 'day') ? this.colors.young : this.colors.middle;
+      return;
+    } 
+
+    this.colorClass = this.checkDate(currentDate, 6) ? this.colors.older : this.colors.old;
   } 
 }

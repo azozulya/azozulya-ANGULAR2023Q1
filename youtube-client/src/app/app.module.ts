3675bd03d28a48b3df280c4e4de91b46f42ae470
azozulya <bbbom@mail.ru>
2023-04-13 21:43:32 +0300

    feat: ngrx install

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -8,10 +8,21 @@ import { CoreModule } from './core/core.module';
 import { DataService } from './core/services/data.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { ApiInterceptor } from './core/interceptor/api.interceptor';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './redux/reducers';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+  ],
   providers: [DataService, AuthService, { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true }],
   bootstrap: [AppComponent],
 })
